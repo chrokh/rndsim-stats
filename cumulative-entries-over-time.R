57e6a9f4118da6ca07708393ea69a92478c1a28d
chrokh <christopher.okhravi@gmail.com>
2017-05-11 18:10:18 +0200

    Port mean cumulative entries over time plot

---
+++ cumulative-entries-over-time.R
@@ -0,0 +1,100 @@
+source('shared.R')
+library(RColorBrewer)
+
+
+# ARGUMENTS
+# ==================
+args <-
+  parseArgs(list(
+                 make_option(
+                             c('-i', '--input'),
+                             default =NULL,
+                             help    ='dataset file path',
+                             metavar ='file'),
+                 make_option(
+                             c('-o', '--output'),
+                             default ='plot.png',
+                             help    ='output file name [default= %default]',
+                             metavar ='file'),
+                 make_option(
+                             c('-c', '--cache'),
+                             default = '.',
+                             help    = 'cache folder [default= %default]',
+                             metavar = 'folder'),
+                 make_option(
+                             c('-l', '--label'),
+                             default = '',
+                             help    = 'dataset identifier [default= %default]',
+                             metavar = 'character'),
+                 make_option(
+                             c('--ylimit'),
+                             default = NULL,
+                             help    = 'ylim max of plot [default= %default]',
+                             metavar = 'float')
+                 ),
+            function(args) !is.null(args$input))
+
+
+
+
+# CONFIG
+# ============================================
+intervention_bins <- c(0, 100, 200, 500, 700, 1000, 1500, 2000, 2500, 3000, 4000)
+colorscale  <- brewer.pal(length(intervention_bins), 'RdYlGn')
+
+
+
+
+# PREPARE
+# ============================================
+
+prepare <- function(df) {
+
+  # Bin interventions
+  df <- binInterventions(df, intervention_bins)
+
+  # Cumulate entries per tick
+  all <- data.frame()
+  for (tick in unique(df$TICK)) {
+    print(paste('Grouping tick', tick))
+    single <- subset(df, df$TICK <= tick)
+    single <- groupBy(single, c('RUN', 'intervention_bin_top'))
+    single$year <- tick / 12
+    all <- rbind(all, single)
+  }
+
+  return(all)
+
+}
+df <- getSet(args$input, args$cache, 'cumulative-entries-over-time.csv', prepare)
+
+
+
+
+
+# PLOT
+# ============================================
+
+plotToFile(args$output)
+p <- ddply(df, c('year', 'intervention_bin_top'), summarise, mean_pois = mean(tot_pois))
+p$color  <- colorscale[findInterval(p$intervention_bin_top, unique(p$intervention_bin_top))]
+plot(p$mean_pois ~ p$year,
+     main = paste("Cumulative market entries (mean per run) per year", args$label),
+     col  = p$color,
+     pch  = 19,
+     xlab = "Year",
+     ylab = "Mean cumulative market entries",
+     xlim = c(min(p$year), max(p$year)),
+     ylim = buildLim(NULL, args$ylim),
+     xaxt="n"
+     )
+axis(1, at = seq(from = 0, to = (max(p$year)), by = 1))
+grid(nx = 0, ny = NULL, col = 'darkgray', lty = 'dotted', equilogs = TRUE)
+legend('topleft', levels(factor(p$intervention_bin_top)), pch = 19, col = p$color, bty = 'n', title="Top")
+sub <- subset(p, p$year == max(p$year))
+text(sub$year, sub$mean_pois, sub$intervention_bin_top, cex=0.8, pos=4, font=2, col=sub$color)
+
+for (bin in intervention_bins) {
+  sub <- subset(p, p$intervention_bin_top == bin)
+  lines(sub$mean_pois ~ sub$year, col = sub$color)
+}
