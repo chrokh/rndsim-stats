a43e61b7e4f55be0b58da77d21cc2159ff5241b4
chrokh <christopher.okhravi@gmail.com>
2017-06-16 10:22:47 +0200

    Separate into groups by intervention in broad-entries-by-intervention plot.

--- broad-entries-by-intervention.R
+++ broad-entries-by-intervention.R
@@ -49,28 +49,109 @@ colorscale <- function(df) {
 # PREPARE
 # ============================================
 prepare <- function(df) {
-  df <- ddply(df, c('RUN', 'interventions_tot_size'), summarise,
-              tot_pois = countCompletes(proj_state))
+
+  df <- ddply(df, c('RUN', 'intervention', 'interventions_tot_size'), summarise,
+              tot_pois  = countCompletes(proj_state))
+
   return(df)
 }
+
 df <- getSet(args$input,
              args$cache,
              'broad-entries-by-intervention.csv',
              prepare,
-             c('RUN', 'interventions_tot_size', 'proj_state'))
+             c('RUN',
+               'TICK',
+               'intervention',
+               'interventions_tot_size',
+               'proj_state'
+               ))
 
 
-head(df)
 
 
 
 # PLOT
 # ============================================
 plotToFile(args$output)
-plot(df$tot_pois  ~ as.factor(df$interventions_tot_size),
-     main    = paste('Total entries per intervention size', args$label),
+layout(matrix(c(1, 2, 2, 2, 2), nrow = 1, byrow = TRUE))
+
+
+
+
+# Group observations
+grp1 <- subset(df, df$intervention == 'FDMER' & df$interventions_tot_size > 0)
+grp1$intervention <- 'FD'
+
+grp2 <- subset(df, df$intervention == 'PDMER' & df$interventions_tot_size > 0)
+grp2$intervention <- 'PD'
+
+base <- subset(df, df$interventions_tot_size == 0)
+base$intervention <- 'None'
+
+grps <- rbind(grp1, grp2)
+
+
+
+
+# Calculate sample sizes
+print('Calculating mean sample count')
+samples <- ddply(df, c('intervention', 'interventions_tot_size'), summarise, n = length(RUN))
+meanN <- mean(samples$n)
+
+
+
+# Plot baseline
+plot(base$tot_pois  ~
+     interaction(
+                 as.factor(base$interventions_tot_size),
+                 base$intervention
+                 ),
+     boxfill = c('peachpuff'),
+     main    = '',
+     ylab    = '',
+     xlab    = paste('Mean N per group:', round(meanN, 2)),
+     las     = 2,
+     ylim    = range(min(grps$tot_pois), max(grps$tot_pois))
+     )
+mtext('No Intervention')
+
+
+
+# Plot interventions
+generateColorscale <- colorRampPalette(c('steelblue1', 'steelblue4'))
+categories <- 2
+boxcolors  <- rep(generateColorscale(categories), categories)
+
+plot(grps$tot_pois  ~
+     interaction(
+                 grps$intervention,
+                 as.factor(grps$interventions_tot_size)
+                 ),
+     boxfill = boxcolors,
+     main    = paste('Total entries per Intervention Size', args$label),
      ylab    = 'Number of entries',
      xlab    = '',
      las     = 2,
-     ylim    = buildLim(NULL, args$ylim)
+     ylim    = range(min(grps$tot_pois), max(grps$tot_pois))
      )
+mtext('Partial and Full Delinkage')
+
+group <- seq(from = categories + 0.5,
+             to = categories * (length(unique(df$interventions_tot_size)) - 1),
+             by = categories)
+abline(h = NULL, v = group, col = 'black', lty = 'solid')
+
+
+
+
+# Print sample sizes
+top <- max(grps$tot_pois)
+samples <- subset(samples, samples$interventions_tot_size > 0)
+samples <- samples[order(samples$interventions_tot_size),]
+n <- 1
+for (sample in samples$n) {
+  text(x = n, y = top, labels = paste('N =', sample))
+  n <- n + 1
+}
+
