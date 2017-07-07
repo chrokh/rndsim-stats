30a59d687c24f511fa482a77e22fbac31e88f400
chrokh <christopher.okhravi@gmail.com>
2017-07-07 13:55:54 +0200

    Improve visuals of funnel bar plots and print to multiple pages.

--- paper2/funnel.R
+++ paper2/funnel.R
@@ -120,16 +120,114 @@ preparePush <- function(df) {
 
   new <- data.frame()
   for (ph in unique(df$phase)) {
+    first <- subset(df, df$phase==ph & df$push==0)
     for (psh in sort(unique(df$push))) {
-      before <- subset(df, df$push < psh & df$phase == ph)
-      after  <- subset(df, df$push >= psh & df$phase == ph)
-      this   <- subset(df, df$push == psh & df$phase == ph)
+      before <- subset(df, df$phase==ph & df$push < psh)
+      after  <- subset(df, df$phase==ph & df$push >= psh)
+      this   <- subset(df, df$phase==ph & df$push == psh)
       last  = max(c(max(before$likelihood), 0))
       nxt   = min(this$likelihood)
       delta = max(nxt - last, 0)
       this$delta <- delta
       new <- rbind(new, this)
     }
+  }
+  df <- new
+  print(df)
+
+  print('Make factors')
+  df$phase <- factor(df$phase,
+                     levels = c(
+                                'p5s',
+                                'p4s',
+                                'p3s',
+                                'p2s',
+                                'p1s'))
+  print(df)
+
+  print('Rename factors')
+  df$phase <- revalue(df$phase, c('p1s'='Phase1', 'p2s'='Phase2', 'p3s'='Phase3', 'p4s'='Approval', 'p5s'='Market'))
+
+  print('Convert to table')
+  tbl <- xtabs(df$delta ~ df$push+ df$phase)
+  print(tbl)
+
+  tbl
+}
+
+
+
+
+
+prepareBasePush <- function(df) {
+
+  print('Subset on pull size = 0')
+  df <- subset(df, df$pull == 0 & df$push == 0)
+
+  print('Prepare')
+  df <- prepareSubset(df, c('push'))
+
+  print('Transform wide to long')
+  df <- gather(df, phase, likelihood, p0s:p5s)
+  print(df)
+
+  print('Remove pre-clinical')
+  df <- subset(df, df$phase != 'p0s')
+  print(df)
+
+  df$delta <- df$likelihood
+
+  print('Make factors')
+  df$phase <- factor(df$phase,
+                     levels = c(
+                                'p5s',
+                                'p4s',
+                                'p3s',
+                                'p2s',
+                                'p1s'))
+  print(df)
+
+  print('Rename factors')
+  df$phase <- revalue(df$phase, c('p1s'='Phase1', 'p2s'='Phase2', 'p3s'='Phase3', 'p4s'='Approval', 'p5s'='Market'))
+
+  print('Convert to table')
+  tbl <- xtabs(df$delta ~ df$push+ df$phase)
+  print(tbl)
+
+  tbl
+}
+
+
+
+
+prepareSimplePush <- function(df, minGrantFraction) {
+
+  print('Subset on pull size = 0')
+  df <- subset(df, df$pull == 0)
+
+  print('Split into only either grants or no grants')
+  df <- subset(df, df$push == 0 | df$push >= minGrantFraction)
+  df$push <- ifelse(df$push == 0, 0, 1)
+
+  print('Prepare')
+  df <- prepareSubset(df, c('push'))
+
+  print('Transform wide to long')
+  df <- gather(df, phase, likelihood, p0s:p5s)
+  print(df)
+
+  print('Remove pre-clinical')
+  df <- subset(df, df$phase != 'p0s')
+  print(df)
+
+  new <- data.frame()
+  for (ph in unique(df$phase)) {
+    no = subset(df, df$phase == ph & df$push==0)
+    ys = subset(df, df$phase == ph & df$push==1)
+    delta = max(ys$likelihood) - min(no$likelihood)
+    no$delta <- no$likelihood
+    ys$delta <- delta
+    new <- rbind(new, no, ys)
   }
   df <- new
   print(df)
@@ -165,7 +263,7 @@ preparePull <- function(df, mertype) {
   df <- subset(df, df$push == 0)
 
   print(paste('Subset on mer =', mertype))
-  df <- subset(df, df$mer == mertype)
+  df <- subset(df, df$mer == mertype | df$pull == 0)
 
   print('Prepare')
   df <- prepareSubset(df, c('pull'))
@@ -221,21 +319,28 @@ preparePull <- function(df, mertype) {
 
 
 
-# PLOT
+# PLOTTING FUNCTION
 # ===========================================
-
-path   = makePath(args$output, 'funnel', 'pdf')
-pdf(path, width = 11, height = 17, pointsize=15)
-layout(matrix(c(1,2,3), nrow=3, byrow=TRUE))
-
 
 doPlot <- function(tbl, name, labels) {
   print('Will plot')
 
-  colors = rainbow(length(labels))
+  colors = colorRampPalette(c(
+                             # 'gray80',
+                              'cornsilk3',
+                              'navajowhite',
+                             # 'chocolate4',
+                              'yellow',
+                             # 'turquoise1',
+                             # 'cyan',
+                              'green3',
+                              'forestgreen',
+                              'darkgreen'
+                              ))(length(labels))
+  #colors = rainbow(length(labels))
+  #colors  = terrain.colors(length(labels))
   #colors = colorRampPalette(c('bisque', 'brown2'))(length(labels))
   #colors = colorRampPalette(c('lightskyblue', 'dodgerblue4'))(length(labels))
-
 
   title = paste('Improvement in Likelihood of Phase Entry (',
                          name,
@@ -248,6 +353,8 @@ doPlot <- function(tbl, name, labels) {
           horiz  = TRUE,
           border = NA,
           main   = '',
+          las    = 2,
+          xlim   = c(0, .8), # Hard-coded
           xaxt   = 'n',
           xlab   = 'Likelihood (%)'
           )
@@ -257,6 +364,12 @@ doPlot <- function(tbl, name, labels) {
   xmarks <- seq(0, 100, by=5)
   xticks <- seq(0, 1,   by=0.05)
   axis(1, at=xticks, labels=xmarks)
+
+  ## Vertical lines
+  xminor <- seq(0.025, 1, by=0.025)
+  xmajor <- seq(0, 1, by=0.05)
+  abline(v=xminor, col='gray', lty=3)
+  abline(v=xmajor, col='gray', lty=2)
 
 
   legend('bottomright',
@@ -268,9 +381,51 @@ doPlot <- function(tbl, name, labels) {
          col     = colors)
 }
 
-df$pushPercentage <- df$push * 100
-doPlot(preparePush(df), 'GRANTS %', sort(unique(df$pushPercentage)))
-doPlot(preparePull(df, 'FDMER'), 'FDMER', sort(unique(df$pull)))
-doPlot(preparePull(df, 'PDMER'), 'PDMER', sort(unique(df$pull)))
 
+
+
+
+
+
+
+
+# PLOTTING FUNCTION
+# ===========================================
+
+df$pushPercentage <- df$push * 100
+
+# Prepare plottable content
+df.1 <- prepareSimplePush(df, 0.1)
+df.2 <- preparePush(df)
+df.3 <- preparePull(df, 'FDMER')
+df.4 <- preparePull(df, 'PDMER')
+#prepareBasePush(df)
+#prepareSimplePush(df, 1)
+
+# ONE PAGER
+path   = makePath(args$output, 'funnel', 'pdf')
+pdf(path, width = 15, height = 22, pointsize=15)
+layout(matrix(c(1,2,3,4), nrow=4, byrow=TRUE))
+doPlot(df.1, 'GRANTS FUNDING ANY %', c('No Grants', 'Grants'))
+doPlot(df.2, 'GRANTS BY %',          sort(unique(df$pushPercentage)))
+doPlot(df.3, 'FDMER',                sort(unique(df$pull)))
+doPlot(df.4, 'PDMER',                sort(unique(df$pull)))
+
+# SEPARATE PAGES
+plotTo <- function(name) {
+  path = makePath(args$output, name, 'pdf')
+  pdf(path, width=15, height=7, pointsize=15)
+}
+
+plotTo('funnel-push-all')
+doPlot(df.1, 'GRANTS FUNDING ANY %', c('No Grants', 'Grants'))
+
+plotTo('funnel-push-each')
+doPlot(df.2, 'GRANTS BY %', sort(unique(df$pushPercentage)))
+
+plotTo('funnel-pull-fdmer')
+doPlot(df.3, 'FDMER', sort(unique(df$pull)))
+
+plotTo('funnel-pull-pdmer')
+doPlot(df.4, 'PDMER', sort(unique(df$pull)))
 
